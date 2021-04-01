6d6f8d46de0af2f5e75fece5bdc48c378a0a6e62
xavierloos <xavier.loos04@gmail.com>
2021-04-01 14:35:17 +0100

    37. The Main Pipeline

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,18 +1,10 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for `Identicon`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |>hash_input
+  end
+  
+  def hash_input(input) do
+    
   end
 end
