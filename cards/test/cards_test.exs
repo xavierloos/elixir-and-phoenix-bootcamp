011f4a08d31c228e59007f210d60cdbaaeab0248
xavierloos <xavier.loos04@gmail.com>
2021-04-01 13:25:36 +0100

    32. Updating Values in a Map

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -32,4 +32,30 @@ defmodule CardsTest do
   # iex(9)> secondary_color
   # "blue"
 
+  # UPDATING VALUES OF MAPS
+  # iex(13)> colors = %{primary: "red"}
+  # %{primary: "red"}
+  # iex(14)> colors.primary = "blue"
+  # ** (CompileError) iex:14: cannot invoke remote function colors.primary/0 inside a match
+  # iex(14)> colors
+  # %{primary: "red"}
+  # iex(15)> Map.put(colors, :primary, "blue") //THIS DON'T MODIFY THE ORIGIINAL MAP
+  # %{primary: "blue"}
+  # iex(16)> colors
+  # %{primary: "red"}
+  # iex(17)> %{colors | primary: "blue"} //THIS DON'T MODIFY THE ORIGIINAL MAP
+  # %{primary: "blue"}
+  # iex(18)> colors
+  # %{primary: "red"}
+  # iex(19)> %{colors | secondary: "red"}
+  # ** (KeyError) key :secondary not found in: %{primary: "red"}
+  #     (stdlib 3.14.1) :maps.update(:secondary, "red", %{primary: "red"})
+  #     (stdlib 3.14.1) erl_eval.erl:259: anonymous fn/2 in :erl_eval.expr/5
+  #     (stdlib 3.14.1) lists.erl:1267: :lists.foldl/3 
+  # iex(19)> Map.put(colors, :secondary, "green") //THIS DON'T MODIFY THE ORIGIINAL MAP
+  # %{primary: "red", secondary: "green"}
+  # iex(20)> colors
+  # %{primary: "red"}
+  # iex(21)> 
+
 end
