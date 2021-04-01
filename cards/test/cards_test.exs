acb87803e426b3ae924fb63c78927fdbd9022a24
xavierloos <xavier.loos04@gmail.com>
2021-04-01 13:09:22 +0100

    31. Introduction to Maps

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -12,4 +12,24 @@ defmodule CardsTest do
     # assert deck != Cards.shuffle(deck)
     refute deck == Cards.shuffle(deck)
   end
+  # MAPS
+  # iex(1)> colors = %{primary: "red"}
+  # %{primary: "red"}
+  # iex(2)> colors.primary
+  # "red"
+  # iex(3)> colors = %{primary: "red", secondary: "blue"}
+  # %{primary: "red", secondary: "blue"}
+  # iex(4)> colors.primary
+  # "red"
+  # iex(5)> colors.secondary
+  # "blue"
+  # iex(6)> color_secondary = colors.secondary
+  # "blue"
+  # iex(7)> color_secondary
+  # "blue"
+  # iex(8)> %{secondary: secondary_color} = colors
+  # %{primary: "red", secondary: "blue"}
+  # iex(9)> secondary_color
+  # "blue"
+
 end
