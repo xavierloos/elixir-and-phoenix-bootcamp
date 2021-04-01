21c28d52504c36224a4dd9b88b1790047f6ca509
xavierloos <xavier.loos04@gmail.com>
2021-04-01 15:04:09 +0100

    39. The Purpose of the Hex List

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -16,4 +16,9 @@ defmodule Identicon do
     # iex(1)> Identicon.main("Javier")
     # [171, 2, 252, 235, 150, 137, 17, 75, 28, 209, 132, 78, 69, 108, 6, 149]
   end
+
+  # Generate the colors: to do this, just take the first three numbers on the list return in the hash which is a RGB form value
+  # Each square in the Identicon grid has a number, if the number is ODD we will NOT color that square.
+
+
 end
