82dd7830675ddc9a967d414673a2582bb75b899a
xavierloos <xavier.loos04@gmail.com>
2021-04-01 14:47:54 +0100

    38. Hashing a String

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -3,8 +3,17 @@ defmodule Identicon do
     input
     |>hash_input
   end
-  
+  # convert the string that's  going to be passed into a series of unique numbers
+  # iex(1)> hash = :crypto.hash(:md5, "banana")
+  # <<114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65>>
+  # iex(2)> Base.encode16(hash) 
+  # "72B302BF297A228A75730123EFEF7C41"
+  # iex(3)> :binary.bin_to_list(hash)
+  # [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]
   def hash_input(input) do
-    
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+    # iex(1)> Identicon.main("Javier")
+    # [171, 2, 252, 235, 150, 137, 17, 75, 28, 209, 132, 78, 69, 108, 6, 149]
   end
 end
