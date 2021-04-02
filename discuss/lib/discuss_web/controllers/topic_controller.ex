98f74432fcec31bf5f78c958af71a6f74c2337cc
xavierloos <xavier.loos04@gmail.com>
2021-04-02 20:56:34 +0100

    68. Phoenix's Code Sharing Model

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -1,6 +1,7 @@
 defmodule Discuss.TopicController do
   # alias // Does NOT modify the current module to hace access to functions, instead makes a link to it when call.
   # import // Copy and paste the functions from another module
+  use DiscussWeb, :controller
   def new() do
     
   end
