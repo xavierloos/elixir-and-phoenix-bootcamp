06ecb8573bc6092c9125395ccf372c5d677d7db0
xavierloos <xavier.loos04@gmail.com>
2021-04-02 20:27:41 +0100

    67. Code Reuse with Import, Alias, and Use

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -1,4 +1,6 @@
 defmodule Discuss.TopicController do
+  # alias // Does NOT modify the current module to hace access to functions, instead makes a link to it when call.
+  # import // Copy and paste the functions from another module
   def new() do
     
   end
