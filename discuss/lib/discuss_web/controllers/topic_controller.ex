342c9d3c3cd45047d432d4bf79cbb76cc23af977
xavierloos <xavier.loos04@gmail.com>
2021-04-02 21:08:52 +0100

    69. Phoenix's Conn Struct

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -2,7 +2,12 @@ defmodule Discuss.TopicController do
   # alias // Does NOT modify the current module to hace access to functions, instead makes a link to it when call.
   # import // Copy and paste the functions from another module
   use DiscussWeb, :controller
-  def new() do
-    
+
+  def new(conn, params) do
+    IO.puts "++++"
+    IO.inspect conn
+    IO.puts "++++"
+    IO.inspect params
+    IO.puts "++++"
   end
 end
