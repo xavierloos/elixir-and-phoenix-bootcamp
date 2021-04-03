c8f042440c69f7e25f3e873d587865c5a9803706
xavierloos <xavier.loos04@gmail.com>
2021-04-03 12:27:43 +0100

    77. Handling Form Submissions

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -2,10 +2,14 @@ defmodule Discuss.TopicController do
   # alias // Does NOT modify the current module to hace access to functions, instead makes a link to it when call.
   # import // Copy and paste the functions from another module
   use DiscussWeb, :controller
-  alias Discuss.Topic
+  alias DiscussWeb.Topic
 
-  def new(conn, params) do
+  def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
-    render conn, "new.html", changeset: changeset
+    render(conn, "new.html", changeset: changeset)
   end
+
+  def create(conn, %{"topic" => topic}) do
+    
+  end 
 end
