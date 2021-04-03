572a225d50d8f0d2d4122c49bb223a562886edf3
xavierloos <xavier.loos04@gmail.com>
2021-04-03 11:46:54 +0100

    74. Generating Changesets for the Form

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -2,8 +2,9 @@ defmodule Discuss.TopicController do
   # alias // Does NOT modify the current module to hace access to functions, instead makes a link to it when call.
   # import // Copy and paste the functions from another module
   use DiscussWeb, :controller
+  alias Discuss.Topic
 
   def new(conn, params) do
-    
+    changeset = Topic.changeset(%Topic{}, %{})
   end
 end
