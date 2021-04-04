f1cf684eeddb33753206fe3d6a4676dc115655d8
xavierloos <xavier.loos04@gmail.com>
2021-04-04 12:56:23 +0100

    91. Linking to the Edit Topic Form

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -34,14 +34,15 @@ defmodule Discuss.TopicController do
   end
 
   def update(conn, %{"id"=>topic_id, "topic"=>topic}) do
-    changeset = Repo.get(Topic, topic_id) |> Topic.changeset(topic)
+    old_topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(old_topic, topic)
     case Repo.update(changeset) do
       {:ok, _topic} ->
         conn
         |> put_flash(:info, "Topic updated")
         |> redirect(to: topic_path(conn, :index))
       {:error changeset} -> 
-        render conn, "edit.html", changeset: changeset
+        render conn, "edit.html", changeset: changeset, topic: old_topic
     end
   end
 end
