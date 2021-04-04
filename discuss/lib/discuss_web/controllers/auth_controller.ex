ca6bc41f1f2e4b43c6aab7449e05f9a78749740a
xavierloos <xavier.loos04@gmail.com>
2021-04-04 20:45:11 +0100

    101. Digesting the OAuth Response

--- discuss/lib/discuss_web/controllers/auth_controller.ex
+++ discuss/lib/discuss_web/controllers/auth_controller.ex
@@ -2,8 +2,7 @@ defmodule DiscussWeb.AuthController do
   use DiscussWeb, :controller
   plug Ueberauth
 
-  def callback(conn, params) do
-    IO.inspect(conn.assigns)
-    IO.inspect(params)
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+    
   end
 end
