19e59e8584891081e781e8f492c6ddfd9a1ff27a
xavierloos <xavier.loos04@gmail.com>
2021-04-04 20:54:04 +0100

    102. The User Changeset

--- discuss/lib/discuss_web/controllers/auth_controller.ex
+++ discuss/lib/discuss_web/controllers/auth_controller.ex
@@ -2,7 +2,11 @@ defmodule DiscussWeb.AuthController do
   use DiscussWeb, :controller
   plug Ueberauth
 
+  alias Discuss.User
+
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
-    
+     user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+     
+     changeset = User.changeset(%User{}, user_params)
   end
 end
