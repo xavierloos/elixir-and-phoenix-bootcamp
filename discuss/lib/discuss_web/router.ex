7f54dad7e8660985018691f39fcbe4b4cbdec8da
xavierloos <xavier.loos04@gmail.com>
2021-04-04 12:33:10 +0100

    89. Defining the Update Route

--- discuss/lib/discuss_web/router.ex
+++ discuss/lib/discuss_web/router.ex
@@ -20,6 +20,7 @@ defmodule DiscussWeb.Router do
     get "/topics/new", TopicController, :new
     post "/topics", TopicController, :create
     get "/topics/:id/edit", TopicController, :edit
+    put "/topics/:id", TopicController, :update
   end
   
 
