eb16718e7855e5b5dfd8d97fdea9810328c1fd65
xavierloos <xavier.loos04@gmail.com>
2021-04-04 11:28:02 +0100

    80. Handling Failed Form Submissions

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -13,7 +13,8 @@ defmodule Discuss.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
     case Repo.insert(changeset) do
       {:ok, post}-> IO.inspect(post)
-      {:error, changeset} -> IO.inspect(changeset)
+      {:error, changeset} -> 
+        render  conn, "new.html", changeset: changeset
     end
   end 
 end
