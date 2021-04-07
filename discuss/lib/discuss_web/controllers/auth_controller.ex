d33eed89d070f8f2981d77cdaca7972a0ad4b3b4
xavierloos <xavier.loos04@gmail.com>
2021-04-07 16:06:30 +0100

    105. Handling OAuth Success

--- discuss/lib/discuss_web/controllers/auth_controller.ex
+++ discuss/lib/discuss_web/controllers/auth_controller.ex
@@ -11,8 +11,18 @@ defmodule DiscussWeb.AuthController do
     signin(conn, changeset)
   end
 
-  def signin(conn, changeset) do
-
+  defp signin(conn, changeset) do
+    case insert_or_update_user(changes) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome back")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: topic_path(conn, :index))
+      {:error, _reason}
+        conn
+        |> put_flash(:error, "Error signing in")
+        |> redirect(to: topic_path(conn, :index))
+    end
   end
 
   defp insert_or_update_user(changeset) do
