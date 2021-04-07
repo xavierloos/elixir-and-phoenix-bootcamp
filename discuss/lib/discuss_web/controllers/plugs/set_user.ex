40b4d856a40daad5d8de99539b0b3fcd38b6fdb4
xavierloos <xavier.loos04@gmail.com>
2021-04-07 16:17:57 +0100

    106. Introduction to Plugs

---
+++ discuss/lib/discuss_web/controllers/plugs/set_user.ex
@@ -0,0 +1,17 @@
+defmodule DiscussWeb.Plugs.SetUser do
+  import Plug.Conn
+  import Phoenix.Controller
+
+  alias Discuss.Repo
+  alias Discuss.User
+  alias Discuss.Repo
+  alias DiscussRouter.Helpers
+
+  def init(_params) do
+    
+  end
+  
+  def call(conn, _params) do
+    
+  end
+end
